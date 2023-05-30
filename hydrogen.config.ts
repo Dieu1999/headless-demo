d3a1b5578d8f1c6b6595c4ae98fd40240ed0f779
Lemonade Developers <69376840+Dieu1999@users.noreply.github.com>
2023-05-30 15:24:36 +0700

    Add routes

--- hydrogen.config.ts
+++
@@ -1,8 +0,0 @@
-// hydrogen.config.ts
-
-export default defineConfig({
-    privateStorefrontToken:
-    /* In this example, the environment variable is stored in `Oxygen.env`.
-       If you're not deploying to Oxygen, then you can choose a different storage location.*/
-      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
-  });
